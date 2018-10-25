50340274eadfdaa765545f9555bcca7f52ef758b
Redmega <angelpiscola@gmail.com>
2018-10-25 01:17:40 -0400

    fix: clean up address a bit

--- src/components/address.tsx
+++ src/components/address.tsx
@@ -2,22 +2,27 @@ import { Address } from "./practitioner";
 import React from "react";
 
 interface AddressComponentProps {
-    address: Address;
+  address: Address;
 }
 
-export default class AddressComponent extends React.Component<AddressComponentProps> {
-    render() {
-        return (
-            <span>
-                <span>Line 1: {this.props.address.line1}</span>
-                <br />
-                {
-                    this.props.address.line2 ?
-                        <span>Line 2: {this.props.address.line2} <br /> </span> :
-                        ""
-                }
-                <span>{this.props.address.city}, {this.props.address.state} {this.props.address.ZIP}</span>
-            </span>
-        );
-    }
+export default class AddressComponent extends React.Component<
+  AddressComponentProps
+> {
+  render() {
+    return (
+      <span>
+        <span>{this.props.address.line1}</span>
+        <br />
+        {this.props.address.line2 && (
+          <span>
+            {this.props.address.line2} <br />
+          </span>
+        )}
+        <span>
+          {this.props.address.city}, {this.props.address.state}
+          {this.props.address.ZIP}
+        </span>
+      </span>
+    );
+  }
 }
